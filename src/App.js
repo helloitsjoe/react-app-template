73f73e965574c00cb65204b51d361200b5e1f729
helloitsjoe <jboyle617@gmail.com>
2021-03-13 20:40:48 -0500

    initial template

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div
+      style={{
+        maxWidth: '700px',
+        textAlign: 'center',
+        margin: '2em auto',
+      }}
+    >
+      <h1>You&apos;re doing great.</h1>
+    </div>
+  );
+};
+
+export default App;
